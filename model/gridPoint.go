6f8cdb66839cd530ca6d491c31b2b36570516e85
G-ghy <1095322940@qq.com>
2023-05-17 21:31:02 +0800

    网格点model模块完善注释

--- model/gridPoint.go
+++ model/gridPoint.go
@@ -4,30 +4,43 @@ import (
 	"time"
 )
 
+// 网格点信息
 type Gridpoint struct {
-	Id           uint64    `gorm:"primary_key;AUTO_INCREMENT;column:id" json:"-"`
-	Coordinate_x float64   `json:"coordinate_x" gorm:"column:coordinate_x;not null" binding:"required"`
-	Coordinate_y float64   `json:"coordinate_y" gorm:"column:coordinate_y;not null" binding:"required"`
-	Coordinate_z float64   `json:"coordinate_z" gorm:"column:coordinate_z;not null" binding:"required"`
-	Place_id     uint64    `json:"-"`
-	Createdate   time.Time `gorm:"column:createdate"`
-	Updatedate   time.Time `gorm:"column:updatedate"`
+	Id           uint64    `gorm:"primary_key;AUTO_INCREMENT;column:id" json:"-"`                       // 网格点ID
+	Coordinate_x float64   `json:"coordinate_x" gorm:"column:coordinate_x;not null" binding:"required"` // 网格点x坐标
+	Coordinate_y float64   `json:"coordinate_y" gorm:"column:coordinate_y;not null" binding:"required"` // 网格点y坐标
+	Coordinate_z float64   `json:"coordinate_z" gorm:"column:coordinate_z;not null" binding:"required"` // 网格点z坐标
+	Place_id     uint64    `json:"-"`                                                                   // 场所ID
+	Createdate   time.Time `gorm:"column:createdate"`                                                   // 创建时间
+	Updatedate   time.Time `gorm:"column:updatedate"`                                                   // 修改时间
 }
 
-// 向数据库插入场所
+// @title	Create
+// @description	结构体方法，向数据库插入场所
+// @auth	高宏宇
+// @receiver	gridpoint *Gridpoint 网格点结构体对象指针
+// @return	error	错误信息
 func (gridpoint *Gridpoint) Create() error {
 	return DB.Mysql.Create(&gridpoint).Error
 }
 
+// @title	GetId
+// @description	结构体方法，提供网格点坐标查询网格点ID
+// @auth	高宏宇
+// @receiver	gridpoint *Gridpoint 网格点结构体对象指针
+// @return	uint64	网格点ID
 func (gridpoint *Gridpoint) GetId() uint64 {
 	t := &Gridpoint{}
-	// TODO 添加查询失败时的处理
 	DB.Mysql.Where("coordinate_x = ? AND coordinate_y = ? AND coordinate_z = ?",
 		gridpoint.Coordinate_x, gridpoint.Coordinate_y, gridpoint.Coordinate_z).Find(&t)
 	return t.Id
 }
 
-// 根据场所id，x，y，z坐标查询某场所的参考点
+// @title	GetGridpoint
+// @description	根据场所id，x，y，z坐标查询网格点
+// @auth	高宏宇
+// @param	coordinate_x float64 网格点x坐标    coordinate_y float64 网格点y坐标    coordinate_z float64 网格点z坐标    place_id uint64 网格点ID
+// @return	*Gridpoint 网格点对象指针    error 错误信息
 func GetGridpoint(coordinate_x float64, coordinate_y float64, coordinate_z float64, place_id uint64) (*Gridpoint, error) {
 	gridpoint := &Gridpoint{}
 	// 查询结果为空报错record not found
@@ -36,18 +49,13 @@ func GetGridpoint(coordinate_x float64, coordinate_y float64, coordinate_z float
 	return gridpoint, db.Error
 }
 
+// @title	GetGridpointById
+// @description	根据ID查询网格点
+// @auth	高宏宇
+// @param	id uint64 网格点ID
+// @return	*Gridpoint 网格点对象指针    error 错误信息
 func GetGridpointById(id uint64) (*Gridpoint, error) {
-	// QUES 为何这里采用指针，可能是因为传参用指针更节省资源
 	t := &Gridpoint{}
-	// TODO 添加查询失败时的处理
 	db := DB.Mysql.Where("id = ?", id).Find(&t)
 	return t, db.Error
 }
-
-// TODO 经纬度和地址验证
-// 结构体属性合法性校验
-// 目前仅校验Username,Password,Usertype
-// func (place *Place) Validate() error {
-// 	validate := validator.New()
-// 	return validate.Struct(user)
-// }
