910641c090d87d4c533bfc40b3a7fd7c5761b274
G-ghy <1095322940@qq.com>
2023-05-18 15:10:46 +0800

    用户model模块完善注释

--- model/user.go
+++ model/user.go
@@ -6,83 +6,106 @@ import (
 	"time"
 )
 
-// TODO validate需要细化
-// TODO 时间充裕考虑自行实现生成salt以及加密过程
-// TODO usertype在注册时需要绑定，但在登录时候无需绑定，是否考虑手动校验
-// User represents a registered user.
-// 标签中的validate即给出该参数的校验规则
-// 添加db.SingularTable(true)后，user即为对应数据库表名
+// 用户信息
 type User struct {
-	Id         uint64    `json:"id" gorm:"primary_key;AUTO_INCREMENT;column:id"`
-	Username   string    `json:"username" gorm:"column:username;not null" binding:"required" validate:"min=1,max=32"`
-	Password   string    `json:"password" gorm:"column:pwdhash;not null" binding:"required" validate:"min=5,max=128"`
-	Usertype   int       `json:"usertype" validate:"required"`
-	Place_id   uint64    `json:"place_id"`
-	Createdate time.Time `json:"createdate" gorm:"column:createdate"`
-	Updatedate time.Time `json:"updatedate" gorm:"column:updatedate"`
+	Id         uint64    `json:"id" gorm:"primary_key;AUTO_INCREMENT;column:id"`                                      // 用户ID
+	Username   string    `json:"username" gorm:"column:username;not null" binding:"required" validate:"min=1,max=32"` // 用户名
+	Password   string    `json:"password" gorm:"column:pwdhash;not null" binding:"required" validate:"min=5,max=128"` // 用户密码
+	Usertype   int       `json:"usertype" validate:"required"`                                                        // 用户类型
+	Place_id   uint64    `json:"place_id"`                                                                            // 用户所在场所ID
+	Createdate time.Time `json:"createdate" gorm:"column:createdate"`                                                 // 创建时间
+	Updatedate time.Time `json:"updatedate" gorm:"column:updatedate"`                                                 // 修改时间
 }
 
+// 用户信息（简要信息）
 type User_Brief struct {
-	Id         uint64    `json:"id" gorm:"primary_key;AUTO_INCREMENT;column:id"`
-	Username   string    `json:"username" gorm:"column:username;not null" binding:"required" validate:"min=1,max=32"`
-	Usertype   int       `json:"usertype" validate:"required"`
-	Place_id   uint64    `json:"place_id"`
-	Createdate time.Time `json:"createdate" gorm:"column:createdate"`
-	Updatedate time.Time `json:"updatedate" gorm:"column:updatedate"`
+	Id         uint64    `json:"id" gorm:"primary_key;AUTO_INCREMENT;column:id"`                                      // 用户ID
+	Username   string    `json:"username" gorm:"column:username;not null" binding:"required" validate:"min=1,max=32"` // 用户名
+	Usertype   int       `json:"usertype" validate:"required"`                                                        // 用户类型
+	Place_id   uint64    `json:"place_id"`                                                                            // 用户所在场所ID
+	Createdate time.Time `json:"createdate" gorm:"column:createdate"`                                                 // 创建时间
+	Updatedate time.Time `json:"updatedate" gorm:"column:updatedate"`                                                 // 修改时间
 }
 
-// TODO User的方法必须要在同一文件中生成，所以Encrypt和Compare必须写在这里，为了增强代码易读性，将具体实现放置于pkg的auth下
-// Encrypt the user password.
+// @title	Encrypt
+// @description	结构体方法，加密用户密码
+// @auth	高宏宇
+// @receiver	user *User 用户结构体对象指针
+// @return	err error 错误信息
 func (user *User) Encrypt() (err error) {
 	user.Password, err = auth.Encrypt(user.Password)
 	return err
 }
 
-// Compare with the plain text password. Returns true if it's the same as the encrypted one (in the `User` struct).
-func (user *User) Compare(password string) (err error) {
-	err = auth.Compare(user.Password, password)
-	return err
-}
-
-// -------------------------------------------------------------------------------------------------
-
-// 向数据库插入用户
+// @title	Create
+// @description	结构体方法，向数据库插入用户
+// @auth	高宏宇
+// @receiver	user *User 用户结构体对象指针
+// @return	err error 错误信息
 func (user *User) Create() error {
 	return DB.Mysql.Create(&user).Error
 }
 
-// 根据id获取user
+// @title	UpdateUserPlaceId
+// @description	结构体方法，修改用户所在场所
+// @auth	高宏宇
+// @receiver	user *User 用户结构体对象指针
+// @param    place_id uint64 场所ID
+// @return	err error 错误信息
+func (user *User) UpdateUserPlaceId(place_id uint64) error {
+	db := DB.Mysql.Model(user).Update("place_id", place_id)
+	return db.Error
+}
+
+// @title	UpdateUserInfo
+// @description	结构体方法，修改用户信息
+// @auth	高宏宇
+// @receiver	user *User 用户结构体对象指针
+// @param    username string 用户名    usertype int 用户类型
+// @return	err error 错误信息
+func (user *User) UpdateUserInfo(username string, usertype int) error {
+	db := DB.Mysql.Model(user).Update(map[string]interface{}{"username": username, "usertype": usertype})
+	return db.Error
+}
+
+// @title	GetUserById
+// @description	根据ID查询用户
+// @auth	高宏宇
+// @param	id uint64 用户ID
+// @return	*User 用户对象指针    error 错误信息
 func GetUserById(id uint64) (*User, error) {
 	user := &User{}
 	db := DB.Mysql.Where("id = ?", id).Find(&user) // 查询结果值存储在user中，返回值是个*gorm.DB
 	return user, db.Error
 }
 
-// 根据username获取user
+// @title	GetUserByUsername
+// @description	根据用户名查询用户
+// @auth	高宏宇
+// @param	username string 用户名
+// @return	*User 用户对象指针    error 错误信息
 func GetUserByUsername(username string) (*User, error) {
 	user := &User{}
-	// TODO 这里的返回值不太能理解为什么是*gorm.DB
 	db := DB.Mysql.Where("username = ?", username).Find(&user) // 查询结果值存储在user中，返回值是个*gorm.DB
 	return user, db.Error
 }
 
-// 修改用户place_id
-func (user *User) UpdateUserPlaceId(place_id uint64) error {
-	db := DB.Mysql.Model(user).Update("place_id", place_id)
-	return db.Error
-}
-
-func (user *User) UpdateUserInfo(username string, usertype int) error {
-	db := DB.Mysql.Model(user).Update(map[string]interface{}{"username": username, "usertype": usertype})
-	return db.Error
-}
-
+// @title	GetUserByPlaceId
+// @description	查询场所全部用户
+// @auth	高宏宇
+// @param	place_id int 待查询场所ID
+// @return	[]*User 用户对象指针数组    error 错误信息
 func GetUserByPlaceId(place_id int) ([]*User, error) {
 	user_list := make([]*User, 0)
 	db := DB.Mysql.Where("place_id = ?", place_id).Find(&user_list)
 	return user_list, db.Error
 }
 
+// @title	FilterUserByTime
+// @description	筛选指定场所中指定时间段内的用户
+// @auth	高宏宇
+// @param	place_id int 待查询场所ID    begin_time time.Time 筛选开始时间    end_time time.Time 筛选结束时间
+// @return	[]*User 用户对象指针数组    error 错误信息
 func FilterUserByTime(place_id int, begin_time time.Time, end_time time.Time) ([]*User, error) {
 	user_list := make([]*User, 0)
 
@@ -91,16 +114,13 @@ func FilterUserByTime(place_id int, begin_time time.Time, end_time time.Time) ([
 	return user_list, db.Error
 }
 
+// @title	DeleteUser
+// @description	删除用户
+// @auth	高宏宇
+// @param	id uint64 待删除用户ID
+// @return	error 错误信息
 func DeleteUser(id uint64) error {
 	user := &User{}
 	db := DB.Mysql.Where("id = ?", id).Delete(&user)
 	return db.Error
 }
-
-// TODO 应当验证用户名是否重复
-// 结构体属性合法性校验
-// 目前仅校验Username,Password,Usertype
-// func (user *User) Validate() error {
-// 	validate := validator.New()
-// 	return validate.Struct(user)
-// }
